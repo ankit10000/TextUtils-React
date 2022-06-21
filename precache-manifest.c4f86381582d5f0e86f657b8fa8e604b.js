3dbf472b9dd97edd80d7d9735d5e996f39516d02
ankit10000 <ankitjangid9197@gmail.com>
2022-06-21 13:56:31 +0530

    Updates

---
+++ precache-manifest.c4f86381582d5f0e86f657b8fa8e604b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3451b051b990bd31192",
+    "url": "/TextUtils-React/static/css/main.8def315b.chunk.css"
+  },
+  {
+    "revision": "b3451b051b990bd31192",
+    "url": "/TextUtils-React/static/js/main.b3451b05.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/TextUtils-React/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils-React/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "3427182f8ed14c1e7cc2",
+    "url": "/TextUtils-React/static/js/runtime~main.3427182f.js"
+  },
+  {
+    "revision": "044056c6f9a46427c54d401f00bcbf4b",
+    "url": "/TextUtils-React/index.html"
+  }
+];
